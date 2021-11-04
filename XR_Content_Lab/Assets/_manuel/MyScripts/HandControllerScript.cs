3a97c10649ae819a79134ae07cd521dd8cc2f1ae
Manuel Illanes <rmib200@gmail.com>
2021-11-04 11:09:00 -0400

    XR hands progress

---
+++ XR_Content_Lab/Assets/_manuel/MyScripts/HandControllerScript.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class HandControllerScript : MonoBehaviour
+{
+    [SerializeField] InputActionReference gripInputAction;
+    [SerializeField] InputActionReference triggerInputAction;
+
+    Animator handAnimator;
+
+    private void Awake() {
+        handAnimator = GetComponent<Animator>();
+    }
+
+    private void OnEnable() {
+        gripInputAction.action.performed += GripPressed;
+        triggerInputAction.action.performed += TriggerPressed;
+    }
+    
+    private void TriggerPressed(InputAction.CallbackContext obj) {
+        {
+           handAnimator.SetFloat("Trigger", obj.ReadValue<float>());
+        }
+    }
+
+
+     private void GripPressed(InputAction.CallbackContext obj) {
+        {
+           handAnimator.SetFloat("Grip", obj.ReadValue<float>());
+        }
+    }
+    
+    private void OnDisable() {
+        gripInputAction.action.performed -= GripPressed;
+        triggerInputAction.action.performed -= TriggerPressed;
+    }
+    
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
